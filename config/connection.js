957e98ec16ff96150149ff99af28b236d299c10d
Jared Henderson <jaredmhenderson@icloud.com>
2018-02-25 21:55:32 -0700

    added heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -21,4 +21,15 @@ connection.connect((err) => {
 
 //Export connection for our ORM to use.
 
+if (process.envJAWSDB_URL) {
+    connection = mysql.createConnection(process.env.envJAWSDB_URL);
+}else {
+    connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+    });
+};
+
 module.exports = connection;    
